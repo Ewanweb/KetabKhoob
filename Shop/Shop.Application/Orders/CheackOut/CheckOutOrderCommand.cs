51c736a870cec7dea6c8f397901981b545cfca8a
mahan mahmodi <mahan01ms@gmail.com>
2025-04-01 18:02:43 +0330

    Add Create Product Use Case

--- Shop/Shop.Application/Orders/CheackOut/CheckOutOrderCommand.cs
+++ Shop/Shop.Application/Orders/CheackOut/CheckOutOrderCommand.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Common.Application;
-using Common.Application.Validation;
-using Common.Application.Validation.FluentValidations;
-using FluentValidation;
 using Shop.Domain.OrderAgg;
 using Shop.Domain.OrderAgg.Repository;
 
@@ -64,49 +61,6 @@ namespace Shop.Application.Orders.CheackOut
             await _repository.Save();
 
             return OperationResult.Success();
-        }
-    }
-
-    public class CheckOutOrderCommandValidator : AbstractValidator<CheckOutOrderCommand>
-    {
-        public CheckOutOrderCommandValidator()
-        {
-            RuleFor(r => r.Name)
-                .NotEmpty().NotNull()
-                .WithMessage(ValidationMessages.required("نام"));
-
-            RuleFor(r => r.Family)
-                .NotEmpty().NotNull()
-                .WithMessage(ValidationMessages.required("نام خانوادگی"));
-
-            RuleFor(r => r.City)
-                .NotEmpty().NotNull()
-                .WithMessage(ValidationMessages.required("شهر"));
-
-            RuleFor(r => r.PostalAddress)
-                .NotEmpty().NotNull()
-                .WithMessage(ValidationMessages.required("شهر"));
-
-            RuleFor(r => r.PostalCode)
-                .NotEmpty().NotNull()
-                .WithMessage(ValidationMessages.required("کد پستی"));
-
-            RuleFor(r => r.Shire)
-                .NotEmpty().NotNull()
-                .WithMessage(ValidationMessages.required("ادرس"));
-
-            RuleFor(r => r.Nationalcode)
-                .NotEmpty().NotNull()
-                .WithMessage(ValidationMessages.required("کدملی"))
-                .MaximumLength(10).WithMessage("کدملی نا معتبر است")
-                .MinimumLength(10).WithMessage("کدملی نا معتبر است")
-                .ValidNationalId();
-
-            RuleFor(r => r.PhoneNumber)
-                .NotEmpty().NotNull()
-                .WithMessage(ValidationMessages.required("شماره موبایل"))
-                .MaximumLength(11)
-                .MinimumLength(11).WithMessage("شماره موبایل نا معتبر است");
         }
     }
 }
